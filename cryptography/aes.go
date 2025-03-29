aba27be25441ec5f55e8ad598cc9eb3e008c1a5f
Francesco Valentini <Francesco.valentini930@gmail.com>
2025-03-29 19:37:18 +0100

    Added GetAESGCM method that initializes the cipher

---
+++ cryptography/aes.go
@@ -0,0 +1,42 @@
+/*
+MIT License
+
+# Copyright (c) 2025 Francesco Valentini
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+package cryptography
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+)
+
+// This method returns the 128-bit block cipher initialized with the given key
+// it wraps the NewGCMWithNonceSize setting the nonce size to 128-bit as recommended
+// by NIST Special Publication 800-38D, Section. 8.2.2
+func GetAESGCM(key []byte) (cipher.AEAD, error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+	aesGCM, err := cipher.NewGCMWithNonceSize(block, 16)
+	return aesGCM, err
+}
---
+++ cryptography/cryptography.go
@@ -0,0 +1,3 @@
+// This package provides all the needed methods, costants and errors to work
+// with cryptography
+package cryptography
