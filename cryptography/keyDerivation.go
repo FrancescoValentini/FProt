29af19296f5d681e98b74c487c6fe071eb20bd86
Francesco Valentini <Francesco.valentini930@gmail.com>
2025-03-29 19:20:07 +0100

    Added the Key Derivation Function

---
+++ cryptography/keyDerivation.go
@@ -0,0 +1,45 @@
+/*
+MIT License
+
+# Copyright (c) 2025 Francesco Valentini
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+package cryptography
+
+import (
+	"crypto/sha256"
+
+	"golang.org/x/crypto/argon2"
+)
+
+// This method uses the Argon2id KDF function to derive a 256-bit
+// key from the password.
+func Derive256BitKey(pwd string, iv []byte) []byte {
+	salt := sha256.Sum256(iv) // SHA-256(IV) is used as salt
+	return argon2.IDKey(
+		[]byte(pwd),
+		salt[:],
+		6,        // Iterations
+		128*1024, // Memory (64MB)
+		4,        // Threads
+		32,       // Key lenght (32 byte = 256 bit)
+	)
+}
