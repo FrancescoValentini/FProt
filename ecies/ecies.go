144aba43648e8fef0ab9a38bb9d27888f9cc7838
Francesco Valentini <Francesco.valentini930@gmail.com>
2025-04-14 21:55:54 +0200

    Added ecies package and related constants

---
+++ ecies/ecies.go
@@ -0,0 +1,6 @@
+// This package provides all the needed methods, constants and errors to work
+// with ecies cryptography
+package ecies
+
+const CURVE_PUBLIC_KEY_LENGTH = 97 // NIST P-384
+const HKDF_INFO_SHARED_SECRET = "fprot-ecies-shared-secret"
