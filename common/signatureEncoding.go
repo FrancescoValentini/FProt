0f639a67dd8459e43134ba1c2885d9d01998fdf9
Francesco Valentini <Francesco.valentini930@gmail.com>
2025-04-19 12:27:50 +0200

    Methods to Encode/Decode a Digital Signature

---
+++ common/signatureEncoding.go
@@ -0,0 +1,25 @@
+package common
+
+import (
+	"encoding/base64"
+	"fmt"
+	"strings"
+)
+
+// Encodes a digital signature in base64url
+func EncodeArmor(signature []byte) string {
+	encode := base64.RawURLEncoding.EncodeToString(signature)
+	return SIGNATURE_HEADER + encode
+}
+
+// Decodes a digital signature in base64url
+func DecodeArmor(signature string) ([]byte, error) {
+	if strings.Contains(signature, SIGNATURE_HEADER) {
+		decoded, err := base64.RawURLEncoding.DecodeString(signature)
+		if err != nil {
+			return nil, fmt.Errorf("%w: %v", ErrInvalidBase64, err)
+		}
+		return decoded, nil
+	}
+	return nil, fmt.Errorf("%w", ErrInvalidDigitalSignatureFormat)
+}
